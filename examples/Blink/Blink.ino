fd03ae3390b151ae1b12ba44c752c2337f5d2771
Fernando Trias <fernando.trias@equiso.com>
2017-01-22 10:39:56 -0500

    New blink example

---
+++ examples/Blink/Blink.ino
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include "Threads.h"
+
+const int LED = 13;
+
+volatile int blinkcode = 0;
+
+void blinkthread() {
+  while(1) {
+    if (blinkcode) {
+      for (int i=0; i<blinkcode; i++) {
+        digitalWrite(LED, HIGH);
+        threads.delay(150);
+        digitalWrite(LED, LOW);
+        threads.delay(150);
+      }
+      blinkcode = 0;
+    }
+    threads.yield();
+  }
+}
+
+void setup() {
+  delay(1000);
+  pinMode(LED, OUTPUT);
+  threads.addThread(blinkthread);
+}
+
+int count = 0;
+
+void loop() {
+  count++;
+  blinkcode = count;
+  delay(5000);
+}
