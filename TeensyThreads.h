53396d6d51404f488ee49d0f34b950dfbff87fd7
Miltiadis Kallianis (MIK) <miltiadis.kallianis@rwth-aachen.de, kallmiltiadis@gmail.com>
2021-08-18 16:50:30 +0200

    implement printThreadInfo feature

--- TeensyThreads.h
+++ TeensyThreads.h
@@ -74,7 +74,7 @@
 /* Enabling debugging information allows access to:
  *   getCyclesUsed()
  */
-// #define DEBUG
+#define DEBUG
 
 extern "C" {
   void context_switch(void);
@@ -186,6 +186,9 @@ public:
   const int DEFAULT_STACK0_SIZE = 10240; // estimate for thread 0?
   int DEFAULT_TICKS = 10;
   static const int DEFAULT_TICK_MICROSECONDS = 100;
+  static const int UTIL_STATE_NAME_DESCRIPTION_LENGTH = 24;
+  static const int UTIL_PRINTF_BUFFER_LENGTH = 256;
+
 
   //ADDED, per task sleep time info
   struct scheduler_info{
@@ -280,6 +283,7 @@ public:
   int id();
   int getStackUsed(int id);
   int getStackRemaining(int id);
+  void printThreadsInfo(void);
 #ifdef DEBUG
   unsigned long getCyclesUsed(int id);
 #endif
